3a298694786086259ec6ae385d6ebbb2d3c2caba
cgens67 <cgenshin587@gmail.com>
2026-06-16 02:09:16 +0800

    Fix JitPack URL formatting in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://www.jitpack.io") }
+        maven { url = uri("https://jitpack.io") }
     }
     dependencies {
         classpath(libs.gradle)
